f98c4eaff364b2f885d094a1a6f1eaa20ec63f28
Thorben Woelk <thorben.woelk@gmail.com>
2018-04-12 14:25:26 +0200

    Write README.

---
+++ README.md
@@ -0,0 +1,20 @@
+Plotting a Timeline from AdWords Auction Insights Data
+======================================================
+
+This R code snippet allows to plot a timeline from Google AdWords Auction Insights data.
+In order to make it run smoothly, please follow the guidelines below.
+
+1. The following packages have to be installed using the `install.packages()` command in your R console:  
+- readr
+- ggplot2
+- scales
+- dplyr
+
+2. In order to set your own brand as a reference category in the plot, open the 'auction_insights_timeline.R' file and adjust all rows where the comment says `##### REPLACE "MYBRAND" WITH YOUR BRAND NAME HERE`.
+
+3. From the AdWords interface, download the auction insights data for the time range of interest. **Make sure this is in CSV format with month as segment and excluding top and summary rows** (just click on 'more options' in order to make those setting).
+
+3. And that should do it! Run the code (e.g. in RStudio) and see if the PDF output is correctly created.
+In case you want to adjust on the size of the plot, you can adjust it in the line where the comment tells you to.
+
+![Auction Insights Plot](https://github.com/ThorbenWoelk/auction_insights_timeline/blob/master/timeline_auction_insights.PNG)
--- auction_insights_timeline.R
+++ auction_insights_timeline.R
@@ -12,11 +12,11 @@ data <- read_csv("data.csv", skip = 0) # download from AdWords interface in CSV 
 data$`Impression share` = as.numeric(sub("%", "", data$`Impression share`))/100
 # order URL levels reverse order alphabetically
 data$`Display URL domain` <- factor(data$`Display URL domain`, levels = rev(unique(data$`Display URL domain`)))
-# replace "You" with brand name 
-levels(data$`Display URL domain`)[levels(data$`Display URL domain`)=="You"] <- "CHANEL"
+# replace "You" with brand name
+levels(data$`Display URL domain`)[levels(data$`Display URL domain`)=="You"] <- "MYBRAND" ##### REPLACE "MYBRAND" WITH YOUR BRAND NAME HERE
 
 # exclude some competitors?
-#levels(data$`Display URL domain`)[levels(data$`Display URL domain`)=="exclude.com"] <- NA 
+#levels(data$`Display URL domain`)[levels(data$`Display URL domain`)=="exclude.com"] <- NA
 
 
 # convert month strings to date
@@ -28,13 +28,13 @@ data <- na.omit(data[order(data$`Display URL domain`, decreasing = FALSE),])
 
 # ==== Layout ====
 lin_color<-scale_color_manual(values=c(
-  "#40fdad", "#333333", "#d8122a","#f1961e", "#00a1bc", "#00aa5a", 
+  "#40fdad", "#333333", "#d8122a","#f1961e", "#00a1bc", "#00aa5a",
   "#61193a", "#e65f2d", "#00596b", "#000000",
   "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000",
   "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"))
 style <- list(theme_minimal(),
               lin_color,
-              theme(axis.title.x=element_blank(), 
+              theme(axis.title.x=element_blank(),
                     # axis.title.y=element_text(size=10, vjust = 3, family = "DINPro-Regular"), # uncomment to specify a different font type for title axis y
                     # legend.text = element_text(size=10, family = "DINPro-Regular"), # uncomment to specify a different font type for text in legend
                     legend.title = element_blank(),
@@ -43,18 +43,18 @@ style <- list(theme_minimal(),
                     panel.grid.major.x = element_blank(),
                     panel.grid.minor.x = element_blank(),
                     panel.grid.minor.y = element_blank(),
-                    axis.ticks=element_blank(), 
+                    axis.ticks=element_blank(),
                     legend.position="top"
               ))
 
 # ==== Plotting ====
 
-pdf(file="auction_insights.pdf", width = 8, height = 3)
-ggplot() + 
-  geom_line(aes(y = data$`Impression share`, x = data$Month, colour = relevel(data$`Display URL domain`, ref = "CHANEL")), size = 0.5) + # set own brand as reference category
-  geom_point(aes(y = data$`Impression share`, x = data$Month, colour = relevel(data$`Display URL domain`, ref = "CHANEL")), size = 1) + # set own brand as reference category
-  style + 
-  ylab("Impression Share") + 
-  scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 week", date_labels = "%b-%y") + 
+pdf(file="auction_insights.pdf", width = 8, height = 3) #### ADJUST HEIGHT AND WIDTH OF THE PLOT HERE
+ggplot() +
+  geom_line(aes(y = data$`Impression share`, x = data$Month, colour = relevel(data$`Display URL domain`, ref = "MYBRAND")), size = 0.5) +  ##### REPLACE "MYBRAND" WITH YOUR BRAND NAME HERE
+  geom_point(aes(y = data$`Impression share`, x = data$Month, colour = relevel(data$`Display URL domain`, ref = "MYBRAND")), size = 1) +  ##### REPLACE "MYBRAND" WITH YOUR BRAND NAME HERE
+  style +
+  ylab("Impression Share") +
+  scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 week", date_labels = "%b-%y") +
   scale_y_continuous(breaks = seq(0.1, 1, 0.1), labels = scales::percent)
 dev.off()
