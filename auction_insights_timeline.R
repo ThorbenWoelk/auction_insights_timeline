5aba795f0dbd39217d4a6da403e91ae0b7387dcc
Thorben Wölk <thorben.woelk@gmail.com>
2018-04-15 12:21:58 +0200

    Clean code. Remove redundancies. Comment.

--- auction_insights_timeline.R
+++ auction_insights_timeline.R
@@ -1,41 +1,64 @@
-library(readr)
+# Plots an auction insights timeline from monthly Auction Insights data in CSV format (pulled from Google Adwords interface).
+#
+# PREREQUISITES
+# =============
+# LIBRARIES: ggplot2, dplyr, scales
+# DATA: An auction insights CSV file segmented by month and saved as 'data.csv' within the same folder as this code file.
+# REFERENCE BRAND: Specify the brand name which replaces the initial "You" in the raw data. 
+# This will be used as a reference category and plotted in green.  
+brand.name <- "MYBRAND" # REPLACE WITH YOUR BRAND NAME HERE
+
+# import libraries
 library(ggplot2)
 library(scales)
 library(dplyr)
-# library(extrafont) # Extrafont let's you change the font types in the output plot. The first time you are using this, you need to run 'font_import()'
-# loadfonts() # uncomment to use different font types you can specify below
 
-data <- read_csv("data.csv", skip = 0) # download from AdWords interface in CSV format with month as segment and excluding top and summary rows
+# CHANGE FONT TYPE
+# Uncomment this to change the font types in the output plot. Need `font_import()` to run the first time.
+# library(extrafont) 
+# loadfonts()
 
-# ==== Preliminary data cleaning ====
-# convert IS strings to percentages
+# import data
+data.file <- "data.csv"
+data <- read.csv(data.file, skip = 0) # download from AdWords interface in CSV format with month as segment and excluding top and summary rows
+
+
+# Preliminary data cleaning
+# =========================
+
+# convert Impression share strings to percentages
 data$`Impression share` = as.numeric(sub("%", "", data$`Impression share`))/100
 # order URL levels reverse order alphabetically
-data$`Display URL domain` <- factor(data$`Display URL domain`, levels = rev(unique(data$`Display URL domain`)))
+data$`Display URL domain` <- factor(data$`Display URL domain`, 
+                                    levels = rev(unique(data$`Display URL domain`)))
 
 # replace "You" with brand name
-levels(data$`Display URL domain`)[levels(data$`Display URL domain`)=="You"] <- "MYBRAND" ##### REPLACE "MYBRAND" WITH YOUR BRAND NAME HERE
+levels(data$`Display URL domain`)[levels(data$`Display URL domain`)=="You"] <- brand.name
 
 
-# exclude some competitors?
+# EXCLUDE COMPETITORS
 #levels(data$`Display URL domain`)[levels(data$`Display URL domain`)=="exclude.com"] <- NA
 
 
 # convert month strings to date
 data$Month = sub(" ", "-", data$Month)
 data$Month = as.Date(paste("01-", data$Month, sep = ""), format = "%d-%B-%Y")
-# delete IS<.1 and sort desc by name
-data <- na.omit(data[order(data$`Display URL domain`, decreasing = FALSE),])
+
+# delete IS<.1
+data <- na.omit(data)
 
 
-# ==== Layout ====
-lin_color<-scale_color_manual(values=c(
+# Layout
+# ======
+
+lin.color<-scale_color_manual(values=c(
   "#40fdad", "#333333", "#d8122a","#f1961e", "#00a1bc", "#00aa5a",
   "#61193a", "#e65f2d", "#00596b", "#000000",
   "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000",
   "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"))
+
 style <- list(theme_minimal(),
-              lin_color,
+              lin.color,
               theme(axis.title.x=element_blank(),
                     # axis.title.y=element_text(size=10, vjust = 3, family = "DINPro-Regular"), # uncomment to specify a different font type for title axis y
                     # legend.text = element_text(size=10, family = "DINPro-Regular"), # uncomment to specify a different font type for text in legend
@@ -49,14 +72,20 @@ style <- list(theme_minimal(),
                     legend.position="top"
               ))
 
-# ==== Plotting ====
+# Plotting
+# ========
 
-pdf(file="auction_insights.pdf", width = 8, height = 3) #### ADJUST HEIGHT AND WIDTH OF THE PLOT HERE
-ggplot() +
-  geom_line(aes(y = data$`Impression share`, x = data$Month, colour = relevel(data$`Display URL domain`, ref = "MYBRAND")), size = 0.5) +  ##### REPLACE "MYBRAND" WITH YOUR BRAND NAME HERE
-  geom_point(aes(y = data$`Impression share`, x = data$Month, colour = relevel(data$`Display URL domain`, ref = "MYBRAND")), size = 1) +  ##### REPLACE "MYBRAND" WITH YOUR BRAND NAME HERE
+# ADJUST HEIGHT AND WIDTH OF THE PLOT BELOW
+pdf(file="auction_insights.pdf", width = 8, height = 3) 
+
+ggplot(data = data, aes(y = data$`Impression share`, 
+           x = Month, 
+           colour = relevel(data$`Display URL domain`, ref = brand.name))) +
+  geom_line(size = 0.5) +  
+  geom_point(size = 1) +  
   style +
   ylab("Impression Share") +
   scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 week", date_labels = "%b-%y") +
   scale_y_continuous(breaks = seq(0.1, 1, 0.1), labels = scales::percent)
+
 dev.off()
